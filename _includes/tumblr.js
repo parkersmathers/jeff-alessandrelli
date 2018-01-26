da3e6fb948272fe7fa2f8dc853a4a76069a9c068
psmathers <parkersmathers@yahoo.com>
2018-01-26 18:01:57 -0500

    Add caching to tumblr fetch

--- _includes/tumblr.js
+++ _includes/tumblr.js
@@ -1,13 +1,37 @@
 (function () {
 
   var list = document.querySelector('.post-list')
-  var url = "https://api.tumblr.com/v2/blog/jeffalessandrelli.tumblr.com/posts?api_key=CBqFvQ7v98cDJ7gbXm4WlAQ4mvKxYN9anw1vzbrQWyPvgIjnGO&limit=5"
+  var url = 'https://api.tumblr.com/v2/blog/jeffalessandrelli.tumblr.com/posts?api_key=CBqFvQ7v98cDJ7gbXm4WlAQ4mvKxYN9anw1vzbrQWyPvgIjnGO&limit=5'
 
-  fetch(url).then(function (result) {
-    return result.json()
-  }).then(function (jsonp) {
-    displayResults(jsonp)
-  })
+  var cachedFetch = function (url, options) {
+    var cacheKey = url
+
+    var cached = sessionStorage.getItem(cacheKey)
+    if (cached !== null) {
+      var response = new Response(new Blob([cached]))
+      return Promise.resolve(response)
+    }
+
+    return fetch(url, options).then(function (response) {
+      if (response.status === 200) {
+        var contentType = response.headers.get('Content-Type')
+        if (contentType && contentType.match(/application\/json/i)) {
+          response.clone().text().then(function (content) {
+            sessionStorage.setItem(cacheKey, content)
+          })
+        }
+      }
+      return response
+    })
+  }
+
+  cachedFetch(url)
+    .then(res => res.json())
+    .then(obj => {
+      displayResults(obj)
+    }).catch(err => {
+      console.log('error getting tumblr data')
+    })
 
   function displayResults(jsonp) {
     var posts = jsonp.response.posts

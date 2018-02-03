8311fb2106b365616488a2409700b37932c13f1e
psmathers <parkersmathers@yahoo.com>
2018-02-03 12:21:40 -0500

    tweak

--- _includes/tumblr.js
+++ _includes/tumblr.js
@@ -37,7 +37,7 @@
     var posts = jsonp.response.posts
 
     posts.forEach(function (post) {
-      console.log(post);
+      // console.log(post);
       var item = document.createElement('li')
       var link = document.createElement('a')
 
